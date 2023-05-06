6ea014ce863a61fb94731527d6f420c19b5a0ee3
Attikash23 <123202790+Attikash23@users.noreply.github.com>
2023-05-05 22:24:29 -0400

    Add files via upload

---
+++ LM 03 code.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <iomanip>
+#include<cmath>
+using namespace std;
+
+const int AT_BAT = 421;
+const int HITS = 123;
+
+
+int main()
+{
+/*int quantity; // contains the amount of items purchased
+float itemPrice; // contains the price of each item
+float totalBill; // contains the total bill.
+cout << setprecision(4) << fixed << showpoint; // formatted output
+cout << "Please input the number of items bought" << endl;
+
+cin>> quantity;
+  cout<< endl;
+cout<< "please input amount items cost"<<endl;
+cin>> itemPrice;
+totalBill = quantity * itemPrice;
+cout<< "you total bill is $" << totalBill<< endl;*/
+
+/*float price1, price2; // The price of 2 items
+int quantity1, quantity2; // The quantity of 2 items
+cout << setprecision(2) << fixed << showpoint;
+cout << "Please input the price and quantity of the first item" << endl;
+cin >> price1;
+cin >> quantity1;
+cout<< "please input the price and quantity of the second item"<<endl;
+  cin >> price2;
+  cin>> quantity2;
+cout << setw(15) << "PRICE" << setw(12) << "QUANTITY\n\n";
+  cout << setw(12) <<price1 << setw(12)<<quantity1<<endl;
+  cout << setw(12) << price2 << setw(12) << quantity2 << endl;*/
+
+//LM 3.3
+  /*float a,b; 
+float hyp; 
+cout << "Please input the value of the two sides" << endl;
+cin >> a >> b;
+cout << "The sides of the right triangle are " << a << " and " << b << endl;
+  hyp = sqrt((pow(a,2)+pow(b,2)));
+  cout << setprecision(2)<< fixed<< hyp;
+cout << "The hypotenuse is " << hyp << endl;*/
+
+  //LM 3.4
+  /*float batAvg;
+batAvg = (float)HITS / (float)AT_BAT;
+  
+cout << "The batting average is " << batAvg << endl;*/
+
+
+  //LM 3.5
+/*double Grade1, Grade2, Grade3;
+  double avg;
+cout<<"please insert the first grade"<< endl;
+  cin>> Grade1;
+cout<<"please insert the second grade"<< endl;
+   cin>> Grade2;
+cout<<"please insert the third grade"<< endl;
+ cin>> Grade3;
+
+  avg = (Grade1 + Grade2 + Grade3) /3;
+  
+cout<< "The average of the 3 grades is "<< setprecision(2)<<fixed<<avg;*/
+
+  
+
+  
+return 0;
+}
